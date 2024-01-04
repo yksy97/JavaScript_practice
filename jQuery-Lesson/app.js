b8ed20bc6d7b96bd1786e74d5d9dd0b8ef637ffc
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-01-04 05:00:52 +0000

    06-end

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -2,6 +2,78 @@
 //   $('body').html('<h1>Hello jQuery!!</h1>');
 // });
 
+// 省略形
+// $(function(){
+// ('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// $('セレクタ').css('background-color','#0000FF');
+
+
+// $(function(){
+//   $('.box1').slideDown(5000);
+// });
+
+// $(function(){
+//   $('.box1').slideUp(5000);
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+// $(function(){
+//   $('.box1').hide(5000);
+// });
+
+// $(function (){
+//   $('.box1').slideDown(function (){
+//     $('.box1').css({
+//       'background-color': '#0000FF',
+//       'width': '200px',
+//       'height': '100px'
+//     }).slideUp(5000);
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+// 【マウスクリックイベントの記述】
+// クリック後に複数のイベントが行われるようにしたいので、onを付ける
+
+// $('.セレクタ名').on('click', function() {
+//   イベント発生時に行われる処理
+// });
+
 $(function(){
- ('body').html('<h1>Hello jQuery!!</h1>');
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
 });
